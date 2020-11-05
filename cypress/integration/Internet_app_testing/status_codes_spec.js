2ae09fbff2f211bafd825a191b0e8c81382161ba
Florian Shllaku <florian.shllaku@gmail.com>
2020-11-05 20:02:53 +0200

    TEST check content on status_codes/200 click

--- cypress/integration/Internet_app_testing/status_codes_spec.js
+++ cypress/integration/Internet_app_testing/status_codes_spec.js
@@ -3,11 +3,25 @@ describe("Form authentication page", () => {
         cy.visit("/status_codes");
     });
 
-    it('should open a secure area with valid credentials', function () {
+    it('URL shoudl show /status_codes/200', function () {
         cy.get("ul > li:nth-child(1) > a").should('have.attr', 'href')
             .then((href) => {
                 cy.visit(href)
             });
         cy.url().should("eq", Cypress.config().baseUrl + "status_codes/200");
     });
+    
+    it('should return a 200 status_code', function () {
+        cy.get('a[href*="status_codes/200"]').click()
+        cy.get('div').should(($div) => {
+            const text = $div.text()
+              
+            expect(text).to.include('200')
+            expect(text).not.to.include('301')
+            expect(text).not.to.include('404')
+            expect(text).not.to.include('500')
+        })
+    });
+
+
 });
