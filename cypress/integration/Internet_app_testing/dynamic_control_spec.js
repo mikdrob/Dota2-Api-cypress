46fd39de4727584bb761b0e603f74a287b4623b8
Ahmed Ruhul Quddos Joyon <ahjoyo@ttu.ee>
2020-11-06 16:06:49 +0200

    cypress test of dynamic_control

---
+++ cypress/integration/Internet_app_testing/dynamic_control_spec.js
@@ -0,0 +1,24 @@
+describe("Form authentication page", () => {
+    beforeEach(() => {
+        cy.visit("/dynamic_controls");
+    });
+
+    it('To tic the check box',function(){
+       cy.get('[type="checkbox"]').check()
+       cy.get('[type="checkbox"]').uncheck() 
+    });
+
+    it('press romove Button',function(){
+        cy.get('#checkbox-example > button').contains('Remove').click();
+        cy.wait(5000);
+        cy.get('#checkbox-example > button').contains('Add').click();
+
+    });
+
+    it('press Enable Button',function(){
+        cy.get('#input-example > button').contains('Enable').click();
+        cy.wait(5000);
+        cy.get('#input-example > button').contains('Disable').click();
+    });
+
+});
